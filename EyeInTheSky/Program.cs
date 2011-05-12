4df33647934ac6026d56b33c49ef77c583860864
Simon Walker <github@helpmebot.org.uk>
2011-05-12 14:42:40 +0100

    create project for eye in the sky monitoring

---
+++ EyeInTheSky/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EyeInTheSky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
