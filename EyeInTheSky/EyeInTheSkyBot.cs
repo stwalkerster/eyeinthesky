a920669a14cd0059136eff96015fa1b4e0a529b9
Simon Walker <github@helpmebot.org.uk>
2011-05-12 15:49:23 +0100

    more work on bot

--- EyeInTheSky/EyeInTheSkyBot.cs
+++ EyeInTheSky/EyeInTheSkyBot.cs
@@ -27,14 +27,33 @@ namespace EyeInTheSky
 
             irc_freenode = new IAL("chat.freenode.net", 8001, "EyeInTheSkyBot", freenodepassword,
                                    "eyeinthesky", "Eye In The Sky");
-
             irc_freenode.connectionRegistrationSucceededEvent += irc_freenode_connectionRegistrationSucceededEvent;
+            irc_freenode.threadFatalError += irc_threadFatalError;
+            irc_freenode.privmsgEvent += irc_freenode_privmsgEvent;
 
             // set up wikimedia connection
 
             irc_wikimedia = new IAL("irc.wikimedia.org", 6667, "EyeInTheSky", "", "eyeinthesky", "Eye In The Sky");
             irc_wikimedia.connectionRegistrationSucceededEvent += irc_wikimedia_connectionRegistrationSucceededEvent;
+            irc_wikimedia.threadFatalError += irc_threadFatalError;
+        }
 
+        static void irc_freenode_privmsgEvent(User source, string destination, string message)
+        {
+            if (destination != "##eyeinthesky")
+                return;
+
+            if(message.ToCharArray()[0] != '=')
+                return;
+
+            string[] tokens = message.Split(' ');
+            
+        }
+
+        static void irc_threadFatalError(object sender, EventArgs e)
+        {
+            irc_freenode.stop();
+            irc_wikimedia.stop();
         }
 
         static void irc_wikimedia_connectionRegistrationSucceededEvent()
