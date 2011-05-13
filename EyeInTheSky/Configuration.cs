e9a0571f1d34046802e37a5d8836277a4293057a
Simon Walker <github@helpmebot.org.uk>
2011-05-13 20:45:22 +0100

    close? idk whats wrong here

--- EyeInTheSky/Configuration.cs
+++ EyeInTheSky/Configuration.cs
@@ -115,6 +115,8 @@ namespace EyeInTheSky
 
                 stalks = StalkList.fetch(navigator.Select("//isky:stalk", xnm));
 
+                sr.Close();
+
                 return true;
             }
             catch (XmlException ex)
