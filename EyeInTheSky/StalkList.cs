d6413f93dcf29a0fbe7375e0e2cea97673417861
Simon Walker <github@helpmebot.org.uk>
2011-05-14 19:59:50 +0100

    clean up old code

--- EyeInTheSky/StalkList.cs
+++ EyeInTheSky/StalkList.cs
@@ -23,38 +23,8 @@ namespace EyeInTheSky
         {
             StalkList list = new StalkList();
 
-            while(xpni.MoveNext())
-            {
-                SimpleStalk s = new SimpleStalk(xpni.Current.GetAttribute("flag", ""));
-                XPathDocument xpd = new XPathDocument(xpni.Current.ReadSubtree());
-                var xpn = xpd.CreateNavigator();
-                XmlNameTable xnt = xpn.NameTable;
-                XmlNamespaceManager xnm = new XmlNamespaceManager(xnt);
-                xnm.AddNamespace("e",
-                                 "https://github.com/stwalkerster/eyeinthesky/raw/master/EyeInTheSky/DataFileSchema.xsd");
-                var it = xpn.Select("//e:user", xnm);
-                if(it.Count == 1)
-                {
-                    it.MoveNext();
-                    s.setUserSearch(it.Current.GetAttribute("value", ""));
-                }
-                
-                it = xpn.Select("//e:page", xnm);
-                if(it.Count == 1)
-                {
-                    it.MoveNext();
-                    s.setPageSearch(it.Current.GetAttribute("value",""));
-                }
 
-                it = xpn.Select("//e:summary", xnm);
-                if(it.Count == 1)
-                {
-                    it.MoveNext();
-                    s.setSummarySearch(it.Current.GetAttribute("value",""));
-                }
 
-                list.Add(s.Flag,s);
-            }
 
             return list;
         }
