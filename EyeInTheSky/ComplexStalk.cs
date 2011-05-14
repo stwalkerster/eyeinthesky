3b8363491f9f94774ab53961e7215ce291d54f45
Simon Walker <github@helpmebot.org.uk>
2011-05-14 19:03:35 +0100

    move classes to their own file

---
+++ EyeInTheSky/ComplexStalk.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+
+namespace EyeInTheSky
+{
+    public class ComplexStalk:Stalk
+    {
+        public ComplexStalk(string flag) : base(flag)
+        {
+        }
+
+        #region Overrides of Stalk
+
+        public override bool match(RecentChange rc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ToXmlFragment(XmlTextWriter xtw)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ EyeInTheSky/SimpleStalk.cs
@@ -0,0 +1,109 @@
+﻿using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace EyeInTheSky
+{
+    public class SimpleStalk : Stalk
+    {
+        public SimpleStalk(string flag) : base(flag)
+        {
+
+        }
+
+        public bool HasUserSearch
+        {
+            get { return hasusercheck; }
+        }
+
+        public bool HasPageSearch
+        {
+            get { return haspagecheck; }
+        }
+
+        public bool HasSummarySearch
+        {
+            get { return hassummarycheck; }
+        }
+
+        private bool hasusercheck;
+        private bool haspagecheck;
+        private bool hassummarycheck;
+
+        private Regex user, page, summary;
+
+        public void setUserSearch(string regex)
+        {
+            hasusercheck = true;
+            user = new Regex(regex);
+        }
+        public void setPageSearch(string regex)
+        {
+            haspagecheck = true;
+            page = new Regex(regex);
+        }
+        public void setSummarySearch(string regex)
+        {
+            hassummarycheck = true;
+            summary = new Regex(regex);
+        }
+
+        public override bool match(RecentChange rc)
+        {
+            if (!(HasUserSearch || HasPageSearch || hassummarycheck))
+                return false;
+
+            if(HasUserSearch && ! user.Match(rc.User).Success)
+            {
+                return false;
+            }
+
+            if(HasPageSearch && ! page.Match(rc.Page).Success)
+            {
+                return false;
+            }
+
+            if (hassummarycheck && !summary.Match(rc.EditSummary).Success)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    
+        public override void ToXmlFragment(XmlTextWriter xtw)
+        {
+            xtw.WriteStartElement("stalk");
+            {
+                xtw.WriteAttributeString("flag", flag);
+
+                if(HasUserSearch)
+                {
+                    xtw.WriteStartElement("user");
+                    {
+                        xtw.WriteAttributeString("value", user.ToString());   
+                    }
+                    xtw.WriteEndElement();
+                }
+
+                if (HasPageSearch)
+                {
+                    xtw.WriteStartElement("page");
+                    {
+                        xtw.WriteAttributeString("value", page.ToString());
+                    }
+                    xtw.WriteEndElement();
+                }
+
+                if (hassummarycheck)
+                {
+                    xtw.WriteStartElement("summary");
+                    {
+                        xtw.WriteAttributeString("value", summary.ToString());
+                    }
+                    xtw.WriteEndElement();
+                }
+            }
+            xtw.WriteEndElement();
+        }
+    }
+}
