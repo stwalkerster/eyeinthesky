ed561dac13ee19fb52f88f874abb31575df6a997
Simon Walker <github@helpmebot.org.uk>
2011-05-16 14:43:44 +0100

    error catching, add flexibility for CSL parsing later

--- EyeInTheSky/Commands/Stalk.cs
+++ EyeInTheSky/Commands/Stalk.cs
@@ -110,14 +110,37 @@ namespace EyeInTheSky.Commands
                 }
                 else
                 {
-                    string xmlfragment = string.Join(" ", tokens);
-                    XmlDocument xd = new XmlDocument();
-                    xd.LoadXml(xmlfragment);
+                    if (tokens.Length < 1)
+                    {
+                        EyeInTheSkyBot.irc_freenode.ircNotice(source.nickname, "More params pls!");
+                        return;
+                    }
+
+                    string type = GlobalFunctions.popFromFront(ref tokens);
+                    if (type == "xml")
+                    {
+                        string xmlfragment = string.Join(" ", tokens);
+                        try
+                        {
+                            XmlDocument xd = new XmlDocument();
+                            xd.LoadXml(xmlfragment);
 
 
-                    StalkNode node = StalkNode.newFromXmlFragment(xd.FirstChild);
-                    ComplexStalk cs = (ComplexStalk)s;
-                    cs.setSearchTree(node);
+                            StalkNode node = StalkNode.newFromXmlFragment(xd.FirstChild);
+                            ComplexStalk cs = (ComplexStalk) s;
+                            cs.setSearchTree(node);
+                            EyeInTheSkyBot.irc_freenode.ircPrivmsg(destination,
+               "Set " + type + " for stalk " + stalk + " with CSL value: " +
+               node);
+
+                        }
+                        catch (XmlException ex)
+                        {
+                            EyeInTheSkyBot.irc_freenode.ircNotice(source.nickname, "XML Error.");
+                        }
+
+                    }
+
                 }
             }
             if (mode == "list")
