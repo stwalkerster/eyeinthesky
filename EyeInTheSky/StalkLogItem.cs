55b1ce58be4d310d1a6162e7f20e39c2c2499bef
Simon Walker <github@helpmebot.org.uk>
2011-05-17 20:57:03 +0100

    stalk log item

---
+++ EyeInTheSky/StalkLogItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace EyeInTheSky
+{
+    class StalkLogItem
+    {
+        private Stalk stalk;
+        private RecentChange rc;
+
+        public StalkLogItem(Stalk s, RecentChange rcitem)
+        {
+            stalk = s;
+            rc = rcitem;
+        }
+
+        public Stalk Stalk
+        {
+            get { return stalk; }
+        }
+
+        public RecentChange RecentChangeItem
+        {
+            get { return rc; }
+        }
+    
+        public XmlElement toXmlFragment(XmlDocument doc, string xmlns)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            
+        }
+    }
+}
