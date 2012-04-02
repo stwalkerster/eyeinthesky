stwalkerster/eyeinthesky

commit f4b647951dcb5139b3e6253a02df68a5b7c93005

fix #17

EyeInTheSky/Commands/Stalk.cs
@@ -144,6 +144,21 @@ namespace EyeInTheSky.Commands
                     EyeInTheSkyBot.irc_freenode.ircNotice(source.nickname, "More params pls!");
                     return;
                 }
+
+                if(tokens[1] == "immediate")
+                {
+                    if (tokens.Length < 3 )
+                    {
+                        EyeInTheSkyBot.irc_freenode.ircNotice(source.nickname, "More params pls!");
+                        return;
+                    }
+
+                    bool imail = bool.Parse(tokens[1]);
+                    EyeInTheSkyBot.config.Stalks[tokens[0]].immediatemail = imail;
+                    EyeInTheSkyBot.irc_freenode.ircPrivmsg(destination,
+                                                           "Set immediatemail attribute on stalk " + tokens[0] + " to " + imail);
+                }
+
                 bool mail = bool.Parse(tokens[1]);
                 EyeInTheSkyBot.config.Stalks[tokens[0]].mail = mail;
                 EyeInTheSkyBot.irc_freenode.ircPrivmsg(destination,

EyeInTheSky/ComplexStalk.cs
@@ -12,7 +12,7 @@ namespace EyeInTheSky
             baseNode = new FalseNode();
         }
 
-        public ComplexStalk(string flag, string timeupd, string timetrig, string mailflag, string descr, string expiryTime) : base(flag)
+        public ComplexStalk(string flag, string timeupd, string timetrig, string mailflag, string descr, string expiryTime, string immediatemail) : base(flag)
         {
             
             if (flag == "")
@@ -20,7 +20,10 @@ namespace EyeInTheSky
             this.flag = flag;
 
             if (!bool.TryParse(mailflag, out this._mail))
-                this._mail = true;
+                this._mail = true; 
+            
+            if (!bool.TryParse(immediatemail, out this._immediatemail))
+                this._immediatemail = false;
 
             this.lastUpdateTime = DateTime.Parse(timeupd);
 
@@ -39,6 +42,7 @@ namespace EyeInTheSky
         private bool _mail = true;
         private string description = "";
         private DateTime _expiryTime = DateTime.MaxValue;
+        private bool _immediatemail;
 
 
         public DateTime LastUpdateTime
@@ -53,7 +57,7 @@ namespace EyeInTheSky
             protected set { lastTriggerTime = value; }
         }
 
-
+        public bool immediatemail { get { return _immediatemail; } set { _immediatemail = value; } }
 
         public bool mail
         {
@@ -113,6 +117,7 @@ namespace EyeInTheSky
             e.SetAttribute("lastupdate", LastUpdateTime.ToString());
             e.SetAttribute("lasttrigger", LastTriggerTime.ToString());
             e.SetAttribute("mail", this.mail.ToString());
+            e.SetAttribute("immediatemail", this.immediatemail.ToString());
             e.SetAttribute("description", this.Description);
             e.SetAttribute("expiry", this.expiryTime.ToString());
 
@@ -135,9 +140,10 @@ namespace EyeInTheSky
             string exptime = time == null ? DateTime.MaxValue.ToString() : time.Value;
 
             string mailflag = element.GetAttribute("mail");
+            string immMail = element.GetAttribute("immediatemail");
             string descr = element.GetAttribute("description");
 
-            ComplexStalk s = new ComplexStalk(element.Attributes["flag"].Value, lastupdtime, lastriggertime, mailflag,descr,exptime);
+            ComplexStalk s = new ComplexStalk(element.Attributes["flag"].Value, lastupdtime, lastriggertime, mailflag, descr, exptime, immMail);
             
             if(element.HasChildNodes)
             {

commit 71ab6af99d7694110e8f3c0c83f7ce49f100f984

fix bug introduced in f4b647951 - fixes #17 again

EyeInTheSky/Commands/Stalk.cs
@@ -153,7 +153,7 @@ namespace EyeInTheSky.Commands
                         return;
                     }
 
-                    bool imail = bool.Parse(tokens[1]);
+                    bool imail = bool.Parse(tokens[2]);
                     EyeInTheSkyBot.config.Stalks[tokens[0]].immediatemail = imail;
                     EyeInTheSkyBot.irc_freenode.ircPrivmsg(destination,
                                                            "Set immediatemail attribute on stalk " + tokens[0] + " to " + imail);