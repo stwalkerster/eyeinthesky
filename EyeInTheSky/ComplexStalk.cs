095afe6ec61ea1d3912ba3d0669bd60ee4919e83
Simon Walker <github@stwalkerster.co.uk>
2012-04-02 19:31:37 +0100

    add actual notification for #17

--- EyeInTheSky/ComplexStalk.cs
+++ EyeInTheSky/ComplexStalk.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net.Mail;
 using System.Xml;
 using EyeInTheSky.StalkNodes;
 
@@ -105,6 +107,9 @@ namespace EyeInTheSky
                 if (this.mail && bool.Parse(EyeInTheSkyBot.config["logstalks"]))
                     EyeInTheSkyBot.config.LogStalkTrigger(flag, rc);
 
+                if (this.immediatemail)
+                    this.immediateMail(rc);
+
                 return true;
             }
             return false;
@@ -160,6 +165,45 @@ namespace EyeInTheSky
                 this.LastUpdateTime = DateTime.Now;
 
             baseNode = node;
+        }
+    
+        public void immediateMail(RecentChange rc)
+        {
+            string arg1 = this.Flag;
+            string arg2 = rc.Page;
+            string arg3 = rc.User;
+            string arg4 = rc.EditSummary;
+            string arg5 = rc.Url;
+            string arg6 = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString();
+            string arg7 = this.Description;
+            string arg8 = this.ToString();
+            string arg9 = rc.EditFlags;
+
+            string template = new StreamReader("Templates/individual.txt").ReadToEnd();
+            template = template
+                .Replace("$1", arg1)
+                .Replace("$2", arg2)
+                .Replace("$3", arg3)
+                .Replace("$4", arg4)
+                .Replace("$5", arg5)
+                .Replace("$6", arg6)
+                .Replace("$7", arg7)
+                .Replace("$8", arg8)
+                .Replace("$9", arg9)
+                ;
+
+            MailMessage mailMessage = new MailMessage
+                                   {
+                                       From = new MailAddress("eyeinthesky@helpmebot.org.uk"),
+                                       Subject = "EyeInTheSky notification",
+                                       Body = template;
+                                   };
+
+            mailMessage.To.Add("stwalkerster@helpmebot.org.uk");
+
+            SmtpClient client = new SmtpClient("mail.helpmebot.org.uk");
+
+            client.Send(mailMessage);
         }
     }
 }
