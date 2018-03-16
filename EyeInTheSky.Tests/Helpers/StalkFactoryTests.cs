2e5d5f095042fafe06d7efe6c55875383621bcac
Simon Walker <github@stwalkerster.co.uk>
2018-03-16 13:42:19 +0000

    build fix

--- EyeInTheSky.Tests/Helpers/StalkFactoryTests.cs
+++ EyeInTheSky.Tests/Helpers/StalkFactoryTests.cs
@@ -40,7 +40,61 @@
         }
 
         [Test]
-        public void ShouldCreateXmlElement()
+        public void ShouldCreateBasicXmlElement()
+        {
+            // arrange
+            var doc = new XmlDocument();
+            var ns = String.Empty;
+            
+            var node = new Mock<IStalkNode>();
+            node.Setup(x => x.ToXmlFragment(doc, ns)).Returns(doc.CreateElement("false"));
+            
+            var stalk = new Mock<IStalk>();
+            stalk.Setup(x => x.SearchTree).Returns(node.Object);
+            stalk.Setup(x => x.Flag).Returns("testflag");
+            stalk.Setup(x => x.IsEnabled).Returns(true);
+            stalk.Setup(x => x.MailEnabled).Returns(true);
+            
+            
+            var sf = new StalkFactory(this.LoggerMock.Object);
+
+            // act
+            var xmlElement = sf.ToXmlElement(stalk.Object, doc, "");
+            
+            // assert
+            Assert.AreEqual("<complexstalk flag=\"testflag\" immediatemail=\"true\" enabled=\"true\"><false /></complexstalk>", xmlElement.OuterXml);
+        }
+
+        [Test]
+        public void ShouldCreateStalkXml()
+        {
+            // arrange
+            var doc = new XmlDocument();
+            var ns = String.Empty;
+            
+            doc.LoadXml("<or><true /><false /></or>");
+            
+            var node = new Mock<IStalkNode>();
+            node.Setup(x => x.ToXmlFragment(doc, ns)).Returns(doc.DocumentElement);
+            
+            var stalk = new Mock<IStalk>();
+            stalk.Setup(x => x.SearchTree).Returns(node.Object);
+            stalk.Setup(x => x.Flag).Returns("testflag");
+            stalk.Setup(x => x.IsEnabled).Returns(true);
+            stalk.Setup(x => x.MailEnabled).Returns(true);
+            
+            
+            var sf = new StalkFactory(this.LoggerMock.Object);
+
+            // act
+            var xmlElement = sf.ToXmlElement(stalk.Object, doc, "");
+            
+            // assert
+            Assert.AreEqual("<complexstalk flag=\"testflag\" immediatemail=\"true\" enabled=\"true\"><or><true /><false /></or></complexstalk>", xmlElement.OuterXml);
+        }
+
+        [Test]
+        public void ShouldCreateCompleteXmlElement()
         {
             // arrange
             var doc = new XmlDocument();
@@ -53,7 +107,11 @@
             stalk.Setup(x => x.SearchTree).Returns(node.Object);
             stalk.Setup(x => x.Flag).Returns("testflag");
             stalk.Setup(x => x.Description).Returns("my description here");
-            
+            stalk.Setup(x => x.IsEnabled).Returns(true);
+            stalk.Setup(x => x.MailEnabled).Returns(true);
+            stalk.Setup(x => x.LastUpdateTime).Returns(new DateTime(2018, 3, 14, 1, 2, 3));
+            stalk.Setup(x => x.LastTriggerTime).Returns(DateTime.MinValue);
+            stalk.Setup(x => x.ExpiryTime).Returns(DateTime.MaxValue);
             
             var sf = new StalkFactory(this.LoggerMock.Object);
 
@@ -61,7 +119,7 @@
             var xmlElement = sf.ToXmlElement(stalk.Object, doc, "");
             
             // assert
-            Assert.AreEqual("<complexstalk flag=\"testflag\" description=\"my description here\"><false /></complexstalk>", xmlElement.OuterXml);
+            Assert.AreEqual("<complexstalk flag=\"testflag\" lastupdate=\"2018-03-14T01:02:03Z\" lasttrigger=\"0001-01-01T00:00:00Z\" immediatemail=\"true\" description=\"my description here\" enabled=\"true\" expiry=\"9999-12-31T23:59:59.9999999Z\"><false /></complexstalk>", xmlElement.OuterXml);
         }
     }
 }
