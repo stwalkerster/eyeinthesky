2eada7984384e16ffda6f4c775816ae81b6c8474
Simon Walker <github@stwalkerster.co.uk>
2018-03-21 13:44:51 +0000

    Unit testing

--- EyeInTheSky.Tests/StalkNodes/NotNodeTest.cs
+++ EyeInTheSky.Tests/StalkNodes/NotNodeTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using EyeInTheSky.Model.Interfaces;
+using Moq;
+using NUnit.Framework;
 
 namespace EyeInTheSky.Tests.StalkNodes
 {
@@ -10,20 +12,24 @@ namespace EyeInTheSky.Tests.StalkNodes
     public class NotNodeTest : SingleChildNodeTestBase<NotNode>
     {
         [Test, TestCaseSource(typeof(NotNodeTest), "TestCases")]
-        public bool DualOperatorTest(StalkNode a)
+        public bool? DualOperatorTest(IStalkNode a)
         {
             var node = new NotNode();
             node.ChildNode = a;
 
-            return node.Match(new RecentChange("", "", "", "", "", 0));
+            return node.Match(new RecentChange("", "", "", "", "", 0), false);
         }
 
         private static IEnumerable TestCases
         {
             get
             {
+                var nullNodeMock = new Mock<IStalkNode>();
+                nullNodeMock.Setup(x => x.Match(It.IsAny<IRecentChange>())).Returns(null);
+
                 yield return new TestCaseData(new TrueNode()).Returns(false);
                 yield return new TestCaseData(new FalseNode()).Returns(true);
+                yield return new TestCaseData(nullNodeMock.Object).Returns(null);
             }
         }
     }
--- EyeInTheSky.Tests/StalkNodes/OrNodeTest.cs
+++ EyeInTheSky.Tests/StalkNodes/OrNodeTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using EyeInTheSky.Model.Interfaces;
+using Moq;
+using NUnit.Framework;
 
 namespace EyeInTheSky.Tests.StalkNodes
 {
@@ -10,23 +12,32 @@ namespace EyeInTheSky.Tests.StalkNodes
     public class OrNodeTest : DoubleChildNodeTestBase<OrNode>
     {
         [Test, TestCaseSource(typeof(OrNodeTest), "TestCases")]
-        public bool DualOperatorTest(StalkNode a, StalkNode b)
+        public bool? DualOperatorTest(IStalkNode a, IStalkNode b)
         {
             var node = new OrNode();
             node.LeftChildNode = a;
             node.RightChildNode = b;
 
-            return node.Match(new RecentChange("", "", "", "", "", 0));
+            return node.Match(new RecentChange("", "", "", "", "", 0), false);
         }
 
         private static IEnumerable TestCases
         {
             get
             {
+                var nullNodeMock = new Mock<IStalkNode>();
+                nullNodeMock.Setup(x => x.Match(It.IsAny<IRecentChange>())).Returns(null);
+
                 yield return new TestCaseData(new TrueNode(), new TrueNode()).Returns(true);
                 yield return new TestCaseData(new FalseNode(), new TrueNode()).Returns(true);
                 yield return new TestCaseData(new TrueNode(), new FalseNode()).Returns(true);
                 yield return new TestCaseData(new FalseNode(), new FalseNode()).Returns(false);
+
+                yield return new TestCaseData(new TrueNode(), nullNodeMock.Object).Returns(true);
+                yield return new TestCaseData(new FalseNode(), nullNodeMock.Object).Returns(null);
+                yield return new TestCaseData(nullNodeMock.Object, new TrueNode()).Returns(true);
+                yield return new TestCaseData(nullNodeMock.Object, new FalseNode()).Returns(null);
+                yield return new TestCaseData(nullNodeMock.Object, nullNodeMock.Object).Returns(null);
             }
         }
     }
