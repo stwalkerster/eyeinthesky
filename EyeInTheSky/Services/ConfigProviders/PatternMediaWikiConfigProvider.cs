4c119053c3f565d09b4214e805364f18cff78f18
Simon Walker <github@stwalkerster.co.uk>
2019-01-24 00:41:04 +0000

    Update MediaWiki library

--- EyeInTheSky/Services/ConfigProviders/PatternMediaWikiConfigProvider.cs
+++ EyeInTheSky/Services/ConfigProviders/PatternMediaWikiConfigProvider.cs
@@ -9,14 +9,18 @@ namespace EyeInTheSky.Services.ConfigProviders
     {
         private readonly string urlReplacement;
         private readonly string userAgent;
+        private readonly string username;
+        private readonly string password;
         private readonly Regex channelMatch;
 
         private readonly Dictionary<string, IMediaWikiConfiguration> cache = new Dictionary<string, IMediaWikiConfiguration>();
         
-        public PatternMediaWikiConfigProvider(string channelMatch, string urlReplacement, string userAgent)
+        public PatternMediaWikiConfigProvider(string channelMatch, string urlReplacement, string userAgent, string username, string password)
         {
             this.urlReplacement = urlReplacement;
             this.userAgent = userAgent;
+            this.username = username;
+            this.password = password;
             this.channelMatch = new Regex(channelMatch);
         }
         
@@ -30,7 +34,7 @@ namespace EyeInTheSky.Services.ConfigProviders
                 }
 
                 var config = new MediaWikiConfiguration(this.channelMatch.Replace(channel, this.urlReplacement),
-                    this.userAgent);
+                    this.userAgent, this.username, this.password);
                 
                 this.cache.Add(channel, config);
 
