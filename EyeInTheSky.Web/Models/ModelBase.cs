b0e93092baa61b4627065f005852ca9b3b9882b6
Simon Walker <github@stwalkerster.co.uk>
2019-08-12 19:29:41 +0100

    ack a few warnings

--- EyeInTheSky.Web/Models/ModelBase.cs
+++ EyeInTheSky.Web/Models/ModelBase.cs
@@ -7,12 +7,24 @@ namespace EyeInTheSky.Web.Models
 
     public class ModelBase
     {
+        // used from Nancy
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
         public IBotUser BotUser { get; set; }
 
+        // used from Nancy
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
         public IAppConfiguration AppConfiguration { get; set; }
+
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        // used from Nancy
         public IIrcClient IrcClient { get; set; }
 
+        // ReSharper disable once CollectionNeverQueried.Global
+        // used from Nancy
         public List<string> Errors { get; set; }
+
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        //used from nancy
         public string Version { get; set; }
     }
 }
