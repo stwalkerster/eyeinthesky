81e67fedb173370fe8993e1cef537791b1c76393
Simon Walker <github@stwalkerster.co.uk>
2024-07-18 19:19:09 +0100

    Support message injection via RabbitMQ
    This is a transitional measure to allow EITS-NG to publish
    notifications without requiring two bots present in channels

--- EyeInTheSky/Services/MqService.cs
+++ EyeInTheSky/Services/MqService.cs
@@ -8,23 +8,29 @@ namespace EyeInTheSky.Services
     using System.Text;
     using Castle.Core.Logging;
     using EyeInTheSky.Services.Interfaces;
-    using Model;
+    using EyeInTheSky.Model;
     using RabbitMQ.Client;
+    using RabbitMQ.Client.Events;
     using RabbitMQ.Client.Exceptions;
+    using Stwalkerster.IrcClient;
+    using Stwalkerster.IrcClient.Interfaces;
 
     public class MqService : IMqService
     {
         private readonly RabbitMqConfiguration mqConfig;
         private readonly ILogger logger;
+        private readonly IIrcClient client;
         private IConnection connection;
 
         private readonly List<IModel> channels = new List<IModel>();
         private IModel managementChannel;
+        private EventingBasicConsumer consumer;
 
-        public MqService(RabbitMqConfiguration mqConfig, ILogger logger)
+        public MqService(RabbitMqConfiguration mqConfig, ILogger logger, IIrcClient freenodeClient)
         {
             this.mqConfig = mqConfig;
             this.logger = logger;
+            this.client = freenodeClient;
         }
 
         public void Start()
@@ -74,6 +80,42 @@ namespace EyeInTheSky.Services
             this.managementChannel.ExchangeDeclare(queue, "topic", true);
             this.managementChannel.QueueDeclare(queue, true, false, false);
             this.managementChannel.QueueBind(queue, queue, "#");
+            
+            var notificationQueue = this.mqConfig.ObjectPrefix + "notification";
+            this.managementChannel.QueueDeclare(notificationQueue, true, false, false);
+            
+            this.consumer = new EventingBasicConsumer(this.managementChannel);
+            this.consumer.Received += this.ConsumerOnReceived;
+            
+            this.managementChannel.BasicConsume(notificationQueue, true, this.consumer);
+
+        }
+
+        private void ConsumerOnReceived(object sender, BasicDeliverEventArgs e)
+        {
+            if (!e.BasicProperties.IsAppIdPresent())
+            {
+                this.logger.WarnFormat("Command message received with no app ID");
+                return;
+            }
+            
+            if (!e.BasicProperties.IsUserIdPresent())
+            {
+                this.logger.ErrorFormat("Command message received with no user ID");
+                return;
+            }
+
+            var content = Encoding.UTF8.GetString(e.Body.ToArray());
+            
+            this.logger.InfoFormat(
+                "Publishing message to {3} IRC target {4} per request of {1}@{2}: {0}",
+                content,
+                e.BasicProperties.UserId,
+                e.BasicProperties.AppId,
+                this.client.ClientName,
+                e.RoutingKey);
+            
+            this.client.SendMessage(e.RoutingKey, content);
         }
         
         public void Stop()
@@ -83,6 +125,8 @@ namespace EyeInTheSky.Services
                 return;
             }
 
+            this.consumer.Received -= this.ConsumerOnReceived;
+            this.consumer = null;
             this.ReturnChannel(this.managementChannel);
             this.managementChannel = null;
             
